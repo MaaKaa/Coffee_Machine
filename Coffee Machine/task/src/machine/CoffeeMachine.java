8e9befb470afbc1307f25d3391dd5a3c4aeeecd7
Marzena <kacprowicz.marzena@gmail.com>
2019-08-30 20:55:50 +0200

    implement stage 6/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,8 +1,74 @@
 package machine;
 
+import java.util.*;
+
 public class CoffeeMachine {
+
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        CoffeeMachineItself coffeeMachine = new CoffeeMachineItself(400, 540, 120, 9, 550);
+        CoffeeMachineItself.CurrentState currentState = CoffeeMachineItself.CurrentState.CHOOSING_AN_ACTION;
+
+        boolean exit = false;
+
+        while (!exit){
+            printMainMenu();
+            Scanner scanner = new Scanner(System.in);
+            String choice = scanner.nextLine();
+
+            switch(choice){
+                case "buy":
+                    printBuyCoffeeMenu();
+
+                    switch(scanner.next()){
+                        case "1":
+                            coffeeMachine.buyEspresso();
+                            break;
+                        case "2":
+                            coffeeMachine.buyLatte();
+                            break;
+                        case "3":
+                            coffeeMachine.buyCappuccino();
+                            break;
+                        case "back":
+                            break;
+                        default:
+                            System.out.println("Wrong instructions. Try again.");
+                            break;
+                    }
+                    break;
+                case "fill":
+                    coffeeMachine.fillCoffeeMachine();
+                    break;
+                case "take":
+                    coffeeMachine.withdrawMoney();
+                    break;
+                case "remaining":
+                    coffeeMachine.printCoffeeMachineInfo();
+                    break;
+                case "exit":
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Wrong instruction. Try again.");
+                    break;
+            }
+        }
+
     }
+
+    public static void printMainMenu(){
+        System.out.println("Write action: buy, fill, take, remaining, exit:");
+    }
+
+    public static void printBuyCoffeeMenu(){
+        System.out.println("What would you like to buy? \n" +
+                "1 - espresso, \n" +
+                "2 - latte, \n" +
+                "3 - cappuccino, \n" +
+                "back - to main menu");
+    }
+
+
+
 }
--- Coffee Machine/task/src/machine/CoffeeMachineItself.java
+++ Coffee Machine/task/src/machine/CoffeeMachineItself.java
@@ -1,4 +1,152 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachineItself {
+    private int water;
+    private int milk;
+    private int coffeeBeans;
+    private int disposableCups;
+    private int money;
+
+    public enum CurrentState {
+        CHOOSING_AN_ACTION, BUYING_COFFEE, FILLING, TAKING_MONEY, CHECKING_REMAINING,
+    }
+
+
+    public CoffeeMachineItself(int water, int milk, int coffeeBeans, int disposableCups, int money) {
+        this.water = water;
+        this.milk = milk;
+        this.coffeeBeans = coffeeBeans;
+        this.disposableCups = disposableCups;
+        this.money = money;
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public void setWater(int water) {
+        this.water = water;
+    }
+
+    public int getMilk() {
+        return milk;
+    }
+
+    public void setMilk(int milk) {
+        this.milk = milk;
+    }
+
+    public int getCoffeeBeans() {
+        return coffeeBeans;
+    }
+
+    public void setCoffeeBeans(int coffeeBeans) {
+        this.coffeeBeans = coffeeBeans;
+    }
+
+    public int getDisposableCups() {
+        return disposableCups;
+    }
+
+    public void setDisposableCups(int disposableCups) {
+        this.disposableCups = disposableCups;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void buyEspresso(){
+        if (this.getWater() < 250){
+            System.out.println("Sorry, not enough water!");
+        } else if (this.getCoffeeBeans() < 16){
+            System.out.println("Sorry, not enough coffee beans!");
+        } else if (this.getDisposableCups() < 1) {
+            System.out.println("Sorry, not enough disposable cups!");
+        } else {
+            System.out.println("I have enough resources, making you a coffee!");
+            water = water - 250;
+            coffeeBeans = coffeeBeans - 16;
+            disposableCups = disposableCups - 1;
+            money = money + 4;
+        }
+
+    }
+
+    public void buyLatte(){
+        if (this.getWater() < 350){
+            System.out.println("Sorry, not enough water!");
+        } else if (this.getMilk() < 75){
+            System.out.println("Sorry, not enough milk!");
+        }else if (this.getCoffeeBeans() < 20){
+            System.out.println("Sorry, not enough coffee beans!");
+        } else if (this.getDisposableCups() < 1) {
+            System.out.println("Sorry, not enough disposable cups!");
+        } else {
+            System.out.println("I have enough resources, making you a coffee!");
+            water = water - 350;
+            milk = milk - 75;
+            coffeeBeans = coffeeBeans - 20;
+            disposableCups =  disposableCups - 1;
+            money = money + 7;
+        }
+    }
+
+    public void buyCappuccino(){
+        if (this.getWater() < 200){
+            System.out.println("Sorry, not enough water!");
+        } else if (this.getMilk() < 100){
+            System.out.println("Sorry, not enough milk!");
+        }else if (this.getCoffeeBeans() < 12){
+            System.out.println("Sorry, not enough coffee beans!");
+        } else if (this.getDisposableCups() < 1) {
+            System.out.println("Sorry, not enough disposable cups!");
+        } else {
+            System.out.println("I have enough resources, making you a coffee!");
+            water = water - 200;
+            milk = milk - 100;
+            coffeeBeans = coffeeBeans - 12;
+            disposableCups = disposableCups - 1;
+            money = money + 6;
+        }
+    }
+
+    public void withdrawMoney(){
+        System.out.println("I gave you: $" + this.getMoney());
+        money = 0;
+    }
+
+    public void fillCoffeeMachine(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water do you want to add:");
+        int waterToAdd = scanner.nextInt();
+        water = this.water + waterToAdd;
+        System.out.println("Write how many ml of milk do you want to add");
+        int milkToAdd = scanner.nextInt();
+        milk = this.milk + milkToAdd;
+        System.out.println("Write how many grams of coffee beans do you want to add");
+        int coffeeBeansToAdd = scanner.nextInt();
+        coffeeBeans = this.coffeeBeans + coffeeBeansToAdd;
+        System.out.println("Write how many disposable cups of coffee do you want to add");
+        int disposableCupsToAdd = scanner.nextInt();
+        disposableCups = this.disposableCups + disposableCupsToAdd;
+    }
+
+    public void printCoffeeMachineInfo(){
+        System.out.println("The coffee machine has: \n"
+                + this.getWater() + " ml of water \n" + this.getMilk() + " ml of milk \n"
+                + this.getCoffeeBeans() + " g of coffee beans \n"
+                + this.getDisposableCups() + " of disposable cups \n"
+                + "$" + this.getMoney() + " of money");
+    }
+
+    public void processUserInput(String userInput){
+
+    }
 }
